4536ed62cc322d72871d8aa7c622ea8525a1e34f
Camilo Gallego <c.gallego3029@gmail.com>
2026-05-08 08:42:19 -0500

    feat: Base del proyecto angular

--- calculadora/src/app/app.spec.ts
+++ calculadora/src/app/app.spec.ts
@@ -22,6 +22,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, fundamentos');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, calculadora');
   });
 });
--- calculadora/src/app/app.ts
+++ calculadora/src/app/app.ts
@@ -7,5 +7,5 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('fundamentos');
+  protected readonly title = signal('calculadora');
 }
