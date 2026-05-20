37a5b00f50631fc64ac0ba2da547602561fcef73
Camilo Gallego <c.gallego3029@gmail.com>
2026-05-19 21:19:44 -0500

    feat: boton con textos dependiendo del estado

--- taller-directivas/src/app/app.ts
+++ taller-directivas/src/app/app.ts
@@ -1,5 +1,7 @@
 import { Component, signal } from '@angular/core';
 
+type Estado = 'pendiente' | 'cargando' | 'listo';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
@@ -7,5 +9,18 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('taller-directivas');
+
+  estado : Estado = 'pendiente';
+
+  users : { name : String }[] = [
+    {name : 'Camilo'},
+    {name : 'Mike wuasauski'},
+    {name : 'jijiuuu'},
+    {name : 'Jesse Pinkman'},
+    {name : 'Gustado Fring'},
+  ];
+
+  estadoBoton() : string {
+    return this.estado === 'listo' ? 'borrar' : 'cargar'
+  }
 }
